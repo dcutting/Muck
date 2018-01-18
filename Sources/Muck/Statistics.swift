603ce12782768169c3b4252aea974fc1a5db0700
Dan Cutting <dan@cutting.io>
2018-01-18 14:53:24 +0000

    Support mean for integer arrays.

--- Sources/Muck/Statistics.swift
+++ Sources/Muck/Statistics.swift
@@ -1,3 +1,7 @@
+protocol DoubleConvertible: Numeric {
+    func asDouble() -> Double
+}
+
 extension Sequence where Element: Numeric {
     var sum: Element {
         return reduce(0, +)
@@ -12,34 +16,11 @@ extension Collection where Element: BinaryFloatingPoint {
     }
 }
 
-//extension Collection where Element: BinaryInteger {
-//    var mean: Double? {
-//        guard !isEmpty else { return nil }
-//        let n = Element((0 as IndexDistance).distance(to: self.count))
-//        let s = Double(sum as! Int)
-//        return s / Double(n as! Int)
-//    }
-//}
-
-protocol DoubleConvertible: Numeric {
-    func asDouble() -> Double
-}
-
-extension Double: DoubleConvertible {
-    func asDouble() -> Double {
-        return Double(self)
-    }
-}
-
-extension Int: DoubleConvertible {
-    func asDouble() -> Double {
-        return Double(self)
-    }
-}
-
-extension Float: DoubleConvertible {
-    func asDouble() -> Double {
-        return Double(self)
+extension Collection where Element: DoubleConvertible, Element: BinaryInteger {
+    var mean: Double? {
+        guard !isEmpty else { return nil }
+        let n = Element((0 as IndexDistance).distance(to: self.count))
+        return sum.asDouble() / n.asDouble()
     }
 }
 
@@ -59,5 +40,23 @@ extension Collection where Element: DoubleConvertible, Element: Comparable, Inde
 extension Int {
     var isEven: Bool {
         return self % 2 == 0
+    }
+}
+
+extension Double: DoubleConvertible {
+    func asDouble() -> Double {
+        return Double(self)
+    }
+}
+
+extension Int: DoubleConvertible {
+    func asDouble() -> Double {
+        return Double(self)
+    }
+}
+
+extension Float: DoubleConvertible {
+    func asDouble() -> Double {
+        return Double(self)
     }
 }
