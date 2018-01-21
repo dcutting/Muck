336143e57688785ca57cf0677d5aa9119d49ff2e
Dan Cutting <dan@cutting.io>
2018-01-21 22:48:49 +0000

    Command line options for path and modules.

--- Package.swift
+++ Package.swift
@@ -11,15 +11,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.18.4")
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.18.4"),
+        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Muck",
-            dependencies: ["SourceKittenFramework"]),
+            dependencies: ["SourceKittenFramework", "Utility"]),
         .testTarget(
             name: "MuckTests",
             dependencies: ["Muck"]),
--- Sources/Muck/main.swift
+++ Sources/Muck/main.swift
@@ -1,15 +1,48 @@
-let finder = SourceKittenFinder(path: "/Users/dcutting/Dropbox/Dan/Code/Gloop", modules: ["Gloop", "Barbaloot"])
-let transformer = Transformer(granularity: .module)
+import Foundation
+import Basic
+import Utility
 
-let files = finder.find()
-let components = transformer.transform(files: files)
+let arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
+let parser = ArgumentParser(usage: "<options>", overview: "A dependency analyser for Swift projects")
+let pathArg: OptionArgument<String> = parser.add(option: "--projectPath", shortName: "-p", kind: String.self, usage: "The root path of the project to analyse (defaults to current directory)")
+let modulesArg: OptionArgument<[String]> = parser.add(option: "--modules", shortName: "-m", kind: [String].self, usage: "The Swift modules to analyse (required)")
 
-let mainSequence = MainSequence(components: components)
+var parsedPath: String?
+var parsedModules: [String]?
+do {
+    let parsedArguments = try parser.parse(arguments)
+    parsedPath = parsedArguments.get(pathArg)
+    parsedModules = parsedArguments.get(modulesArg)
+}
+catch let error as ArgumentParserError {
+    print(error.description)
+}
+catch let error {
+    print(error.localizedDescription)
+}
 
-let reporter = CompoundReporter(reporters: [
-    AbstractnessReporter(),
-    StabilityReporter(),
-    CSVReporter(sortBy: .distance),
-    StatisticsReporter()
-])
-print(reporter.makeReport(for: mainSequence))
+func start(path: String, modules: [String]) {
+
+    let finder = SourceKittenFinder(path: path, modules: modules)
+    let transformer = Transformer(granularity: .module)
+
+    let files = finder.find()
+    let components = transformer.transform(files: files)
+
+    let mainSequence = MainSequence(components: components)
+
+    let reporter = CompoundReporter(reporters: [
+        AbstractnessReporter(),
+        StabilityReporter(),
+        CSVReporter(sortBy: .distance),
+        StatisticsReporter()
+        ])
+    print(reporter.makeReport(for: mainSequence))
+}
+
+if let modules = parsedModules {
+    let path = parsedPath ?? currentWorkingDirectory.asString
+    start(path: path, modules: modules)
+} else {
+    parser.printUsage(on: stdoutStream)
+}
