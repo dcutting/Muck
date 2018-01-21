827567163fef09c73fa26146442c374221f6a823
Dan Cutting <dan@cutting.io>
2018-01-21 22:58:00 +0000

    Command-line option for treating folders as components instead of modules.

--- Sources/Muck/main.swift
+++ Sources/Muck/main.swift
@@ -6,13 +6,16 @@ let arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
 let parser = ArgumentParser(usage: "<options>", overview: "A dependency analyser for Swift projects")
 let pathArg: OptionArgument<String> = parser.add(option: "--projectPath", shortName: "-p", kind: String.self, usage: "The root path of the project to analyse (defaults to current directory)")
 let modulesArg: OptionArgument<[String]> = parser.add(option: "--modules", shortName: "-m", kind: [String].self, usage: "The Swift modules to analyse (required)")
+let byFolderArg: OptionArgument<Bool> = parser.add(option: "--byFolder", shortName: "-f", kind: Bool.self, usage: "Treat folders as components (defaults to modules)")
 
 var parsedPath: String?
 var parsedModules: [String]?
+var parsedByFolder: Bool?
 do {
     let parsedArguments = try parser.parse(arguments)
     parsedPath = parsedArguments.get(pathArg)
     parsedModules = parsedArguments.get(modulesArg)
+    parsedByFolder = parsedArguments.get(byFolderArg)
 }
 catch let error as ArgumentParserError {
     print(error.description)
@@ -21,10 +24,10 @@ catch let error {
     print(error.localizedDescription)
 }
 
-func start(path: String, modules: [String]) {
+func start(path: String, modules: [String], granularity: Transformer.ComponentGranularity) {
 
     let finder = SourceKittenFinder(path: path, modules: modules)
-    let transformer = Transformer(granularity: .module)
+    let transformer = Transformer(granularity: granularity)
 
     let files = finder.find()
     let components = transformer.transform(files: files)
@@ -42,7 +45,9 @@ func start(path: String, modules: [String]) {
 
 if let modules = parsedModules {
     let path = parsedPath ?? currentWorkingDirectory.asString
-    start(path: path, modules: modules)
+    let byFolder = parsedByFolder ?? false
+    let granularity: Transformer.ComponentGranularity = byFolder ? .folder : .module
+    start(path: path, modules: modules, granularity: granularity)
 } else {
     parser.printUsage(on: stdoutStream)
 }
