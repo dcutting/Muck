2f8f65b49f7b19766d6bd0c1847b36e7e6050c82
Dan Cutting <dan@cutting.io>
2018-01-23 11:29:39 +0000

    Use stream to print to stderr.

--- Sources/Muck/Utility/Print.swift
+++ Sources/Muck/Utility/Print.swift
@@ -1,7 +1,5 @@
-import Foundation
+import Basic
 
 func printStdErr(_ message: String) {
-    if let messageData = (message + "\n").data(using: .utf8) {
-        FileHandle.standardError.write(messageData)
-    }
+    stderrStream <<< message <<< "\n"
 }
