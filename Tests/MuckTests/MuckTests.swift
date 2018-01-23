424f81b52bcbc104ca2d0243600f2c682ebb53a8
Dan Cutting <dan@cutting.io>
2018-01-23 13:57:08 +0000

    Fix test.

--- Tests/MuckTests/MuckTests.swift
+++ Tests/MuckTests/MuckTests.swift
@@ -1,15 +1,12 @@
 import XCTest
-import Muck
+@testable import Muck
 
 class MuckTests: XCTestCase {
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let muck = MuckApp()
+        let muck = Raker()
         XCTAssertNotNil(muck)
     }
-
 
     static var allTests = [
         ("testExample", testExample),
