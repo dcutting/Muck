6666cbe3d75127f8d769902799a4983ff72cfa19
Dan Cutting <dan@cutting.io>
2018-01-23 11:39:52 +0000

    Tidy up.

--- Sources/Muck/Muck/ArgumentsBuilder.swift
+++ Sources/Muck/Muck/ArgumentsBuilder.swift
@@ -10,54 +10,56 @@ enum ArgumentsBuilderError: Error {
 
 class ArgumentsBuilder {
 
-    let parser = ArgumentParser(usage: "<options>", overview: "A dependency analyser for Swift projects")
+    private let parser = ArgumentParser(usage: "<options>", overview: "A dependency analyser for Swift projects")
 
     func parse(arguments: [String]) -> Muck.Arguments {
 
         do {
-            let workspaceArg: OptionArgument<String> = parser.add(option: "--workspace", shortName: "-w", kind: String.self, usage: "The Xcode workspace (specify either workspace or project but not both)")
-            let projectArg: OptionArgument<String> = parser.add(option: "--project", shortName: "-p", kind: String.self, usage: "The Xcode project (specify either workspace or project but not both)")
-            let schemeArg: OptionArgument<String> = parser.add(option: "--scheme", shortName: "-s", kind: String.self, usage: "The Xcode scheme (required if workspace is specified)")
-            let targetArg: OptionArgument<String> = parser.add(option: "--target", shortName: "-t", kind: String.self, usage: "The Xcode target (required if project is specified)")
-            let modulesArg: OptionArgument<[String]> = parser.add(option: "--modules", shortName: "-m", kind: [String].self, usage: "The modules to analyse (required)")
-            let byFolderArg: OptionArgument<Bool> = parser.add(option: "--byFolder", shortName: "-f", kind: Bool.self, usage: "Treat folders as components (by default, modules are treated as components)")
-            let verboseArg: OptionArgument<Bool> = parser.add(option: "--verbose", shortName: "-v", kind: Bool.self, usage: "Verbose logging")
-
-            var parsedWorkspace: String?
-            var parsedProject: String?
-            var parsedScheme: String?
-            var parsedTarget: String?
-            var parsedModules: [String]?
-            var parsedByFolder: Bool?
-            var parsedVerboseArg: Bool?
+            let workspaceArg: OptionArgument<String> =
+                parser.add(option: "--workspace", shortName: "-w", kind: String.self, usage: "The Xcode workspace (specify either workspace or project but not both)")
+            let projectArg: OptionArgument<String> =
+                parser.add(option: "--project", shortName: "-p", kind: String.self, usage: "The Xcode project (specify either workspace or project but not both)")
+            let schemeArg: OptionArgument<String> =
+                parser.add(option: "--scheme", shortName: "-s", kind: String.self, usage: "The Xcode scheme (required if workspace is specified)")
+            let targetArg: OptionArgument<String> =
+                parser.add(option: "--target", shortName: "-t", kind: String.self, usage: "The Xcode target (required if project is specified)")
+            let modulesArg: OptionArgument<[String]> =
+                parser.add(option: "--modules", shortName: "-m", kind: [String].self, usage: "The modules to analyse (required)")
+            let byFolderArg: OptionArgument<Bool> =
+                parser.add(option: "--byFolder", shortName: "-f", kind: Bool.self, usage: "Treat folders as components (by default, modules are treated as components)")
+            let verboseArg: OptionArgument<Bool> =
+                parser.add(option: "--verbose", shortName: "-v", kind: Bool.self, usage: "Verbose logging")
 
             let arguments = Array(arguments.dropFirst())
             let parsedArguments = try parser.parse(arguments)
 
-            parsedWorkspace = parsedArguments.get(workspaceArg)
-            parsedProject = parsedArguments.get(projectArg)
-            parsedScheme = parsedArguments.get(schemeArg)
-            parsedTarget = parsedArguments.get(targetArg)
-            parsedModules = parsedArguments.get(modulesArg)
-            parsedByFolder = parsedArguments.get(byFolderArg)
-            parsedVerboseArg = parsedArguments.get(verboseArg)
+            let parsedWorkspace = parsedArguments.get(workspaceArg)
+            let parsedProject = parsedArguments.get(projectArg)
+            let parsedScheme = parsedArguments.get(schemeArg)
+            let parsedTarget = parsedArguments.get(targetArg)
+            let parsedModules = parsedArguments.get(modulesArg)
+            let parsedByFolder = parsedArguments.get(byFolderArg)
+            let parsedVerboseArg = parsedArguments.get(verboseArg)
 
             guard let moduleNames = parsedModules else {
                 throw ArgumentsBuilderError.noModulesSpecified
             }
+
+            let isVerbose = parsedVerboseArg ?? false
+
+            let byFolder = parsedByFolder ?? false
+            let granularityStrategy: GranularityStrategy = byFolder ? FolderGranularityStrategy() : ModuleGranularityStrategy()
 
             var hasWorkspace = false
             var hasScheme = false
             var hasProject = false
             var hasTarget = false
             var path = ""
-            let byFolder = parsedByFolder ?? false
-            let granularityStrategy: GranularityStrategy = byFolder ? FolderGranularityStrategy() : ModuleGranularityStrategy()
             var xcodeBuildArguments = [String]()
+
             if let workspace = parsedWorkspace {
                 xcodeBuildArguments.append(contentsOf: ["-workspace", workspace])
-                let workspacePath = URL(fileURLWithPath: workspace)
-                path = workspacePath.deletingLastPathComponent().path
+                path = findPath(forWorkspaceOrProject: workspace)
                 hasWorkspace = true
             }
             if let scheme = parsedScheme {
@@ -66,8 +68,7 @@ class ArgumentsBuilder {
             }
             if let project = parsedProject {
                 xcodeBuildArguments.append(contentsOf: ["-project", project])
-                let projectPath = URL(fileURLWithPath: project)
-                path = projectPath.deletingLastPathComponent().path
+                path = findPath(forWorkspaceOrProject: project)
                 hasProject = true
             }
             if let target = parsedTarget {
@@ -75,8 +76,6 @@ class ArgumentsBuilder {
                 hasTarget = true
             }
             let componentNameStrategy: ComponentNameStrategy = byFolder ? FilePathComponentNameStrategy(rootPath: path + "/") : ModuleComponentNameStrategy()
-
-            let isVerbose = parsedVerboseArg ?? false
 
             if isVerbose {
                 printStdErr(path)
@@ -100,7 +99,12 @@ class ArgumentsBuilder {
         }
     }
 
-    func exitWithUsage() -> Never {
+    private func findPath(forWorkspaceOrProject workspaceOrProject: String) -> String {
+        let path = URL(fileURLWithPath: workspaceOrProject)
+        return path.deletingLastPathComponent().path
+    }
+
+    private func exitWithUsage() -> Never {
         parser.printUsage(on: stderrStream)
         exit(1)
     }
