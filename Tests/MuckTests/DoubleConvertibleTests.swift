0c3a1050ea0cec55bd65ebcb40982fcd8d02cf7a
Dan Cutting <dan@cutting.io>
2018-01-24 15:04:13 +0000

    Test DoubleConvertible.

---
+++ Tests/MuckTests/DoubleConvertibleTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import Muck
+
+class DoubleConvertible: XCTestCase {
+
+    func test_doubleAsDouble() {
+        let x: Double = 5.3
+        let actual: Double = x.asDouble()
+        let expected: Double = 5.3
+        XCTAssertEqual(expected, actual, accuracy: 0.001)
+    }
+
+    func test_intAsDouble() {
+        let x: Int = 5
+        let actual: Double = x.asDouble()
+        let expected: Double = 5.0
+        XCTAssertEqual(expected, actual, accuracy: 0.001)
+    }
+
+    func test_floatAsDouble() {
+        let x: Float = 5.3
+        let actual: Double = x.asDouble()
+        let expected: Double = 5.3
+        XCTAssertEqual(expected, actual, accuracy: 0.001)
+    }
+}
