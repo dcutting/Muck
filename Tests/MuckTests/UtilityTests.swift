c06980cad767688f3813f56a5a06e4a56bf5b172
Dan Cutting <dan@cutting.io>
2018-01-24 13:43:37 +0000

    Test mean.

--- Tests/MuckTests/UtilityTests.swift
+++ Tests/MuckTests/UtilityTests.swift
@@ -5,11 +5,31 @@ class UtilityTests: XCTestCase {
 
     func test_sum_emptySequence_returns0() {
         let input = [Int]()
-        XCTAssertEqual(input.sum, 0)
+        XCTAssertEqual(0, input.sum)
     }
 
     func test_sum() {
-        XCTAssertEqual([1,-2,4].sum, 3)
+        XCTAssertEqual(3, [1, -2, 4].sum)
+    }
+
+    func test_mean_emptyFloatCollection_returnsNil() {
+        let input = [Float]()
+        XCTAssertNil(input.mean)
+    }
+
+    func test_mean_emptyIntCollection_returnsNil() {
+        let input = [Int]()
+        XCTAssertNil(input.mean)
+    }
+
+    func test_mean_floatCollection() {
+        let actual = [3.4, 5.3, 9.9].mean!
+        XCTAssertEqual(6.2, actual, accuracy: 0.001)
+    }
+
+    func test_mean_intCollection() {
+        let actual = [3, 4, 6].mean!
+        XCTAssertEqual(4.333, actual, accuracy: 0.001)
     }
 
     func test_isEven_oddInput_returnsFalse() {
