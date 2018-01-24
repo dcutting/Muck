6f3e499a7488f64ad7a95141d7161fabf4db221f
Dan Cutting <dan@cutting.io>
2018-01-24 14:46:24 +0000

    Mean, median, stddev tests.

--- Tests/MuckTests/UtilityTests.swift
+++ Tests/MuckTests/UtilityTests.swift
@@ -23,13 +23,46 @@ class UtilityTests: XCTestCase {
     }
 
     func test_mean_floatCollection() {
-        let actual = [3.4, 5.3, 9.9].mean!
+        let input: [Float] = [3.4, 5.3, 9.9]
+        let actual: Float = input.mean!
+        XCTAssertEqual(6.2, actual, accuracy: 0.001)
+    }
+
+    func test_mean_doubleCollection() {
+        let input: [Double] = [3.4, 5.3, 9.9]
+        let actual: Double = input.mean!
         XCTAssertEqual(6.2, actual, accuracy: 0.001)
     }
 
     func test_mean_intCollection() {
         let actual = [3, 4, 6].mean!
         XCTAssertEqual(4.333, actual, accuracy: 0.001)
+    }
+
+    func test_median_empty_returnsNil() {
+        let input = [Int]()
+        XCTAssertNil(input.median)
+    }
+
+    func test_median_oddCount_returnsMiddleElement() {
+        let input = [4, 5, 3]
+        XCTAssertEqual(4, input.median)
+    }
+
+    func test_median_evenCount_returnsMeanOfMiddleElements() {
+        let actual = [4, 5, 3, 9].median!
+        XCTAssertEqual(4.5, actual, accuracy: 0.001)
+    }
+
+    func test_standardDeviation_empty_returnsNil() {
+        let input = [Float]()
+        XCTAssertNil(input.standardDeviation)
+    }
+
+    func test_standardDeviation() {
+        let input: [Float] = [3.4, 5.3, 9.9]
+        let actual = input.standardDeviation!
+        XCTAssertEqual(2.7288581250528, actual, accuracy: 0.00001)
     }
 
     func test_isEven_oddInput_returnsFalse() {
