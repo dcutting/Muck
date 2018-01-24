1bb86ff391b510158d4fddb283bf22806c1cc0d9
Dan Cutting <dan@cutting.io>
2018-01-24 15:13:56 +0000

    Test ModuleGranularityStrategy.

---
+++ Tests/MuckTests/ModuleGranularityStrategyTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import Muck
+
+class ModuleGranularityStrategyTests: XCTestCase {
+
+    var sut: ModuleGranularityStrategy!
+
+    override func setUp() {
+        sut = ModuleGranularityStrategy()
+    }
+
+    func test_findComponentID_returnsModuleName() {
+        let sourceFile = SourceFile(path: any(), module: "my_module", declarations: any(), references: any())
+        XCTAssertEqual("my_module", sut.findComponentID(for: sourceFile))
+    }
+
+    func test_description() {
+        XCTAssertEqual("treat modules as components", sut.description)
+    }
+}
+
+func any() -> String {
+    return "dummy"
+}
+
+func any() -> [Entity] {
+    return []
+}
