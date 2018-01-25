d8e8141243761dee035e0bdfa7c3c8971c69e96a
Dan Cutting <dan@cutting.io>
2018-01-25 00:11:51 +0000

    DeclarationReporterTests.

---
+++ Tests/MuckTests/DeclarationReporterTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import Muck
+
+class DeclarationReporterTests: XCTestCase {
+
+    func test_name() {
+        let sut = DeclarationReporter()
+        XCTAssertEqual("Declarations", sut.name)
+    }
+
+    func test_makeReport() {
+        let sut = DeclarationReporter()
+
+        var loraxDeclarations = Declarations()
+        loraxDeclarations.addAbstract("GluppityGlup")
+        loraxDeclarations.addAbstract("SchloppitySchlopp")
+        loraxDeclarations.addConcrete("SwomeeSwan")
+        loraxDeclarations.addConcrete("Barbaloot")
+        let lorax = Component(name: "Lorax", declarations: loraxDeclarations, references: any())
+
+        var catDeclarations = Declarations()
+        catDeclarations.addConcrete("Hat")
+        let cat = Component(name: "Cat", declarations: catDeclarations, references: any())
+
+        let components = [lorax, cat]
+        let mainSequence = MainSequence(components: components)
+        let actual = sut.makeReport(for: mainSequence)
+        let expected = """
+Lorax
+  - [A] GluppityGlup
+  - [A] SchloppitySchlopp
+  - SwomeeSwan
+  - Barbaloot
+Cat
+  - Hat
+"""
+        XCTAssertEqual(expected, actual)
+    }
+}
