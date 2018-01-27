db2b86f1cb022073773a1a55c9ce7e6ce7ec81f8
Dan Cutting <dan@cutting.io>
2018-01-27 20:53:08 +0000

    FileGranularityStrategyTests.

---
+++ Tests/MuckTests/FileGranularityStrategyTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import Muck
+
+class FileGranularityStrategyTests: XCTestCase {
+
+    var sut: FileGranularityStrategy!
+
+    override func setUp() {
+        sut = FileGranularityStrategy()
+    }
+
+    func test_description() {
+        XCTAssertEqual("treat files as components", sut.description)
+    }
+
+    func test_findComponentID_returnsWholePath() {
+        let path = "/path/to/component/file.swift"
+        let sourceFile = SourceFile(path: path, module: any(), declarations: any(), references: any())
+        XCTAssertEqual("/path/to/component/file.swift", sut.findComponentID(for: sourceFile))
+    }
+}
