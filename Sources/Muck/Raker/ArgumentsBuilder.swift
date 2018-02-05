0a44682ba2078f2781a049f7380c8d5bc7d2fff4
Dan Cutting <dan@cutting.io>
2018-02-05 00:20:55 +0000

    Extract project argument builder.

--- Sources/Muck/Raker/ArgumentsBuilder.swift
+++ Sources/Muck/Raker/ArgumentsBuilder.swift
@@ -38,8 +38,9 @@ class ArgumentsBuilder {
 
     func parse(arguments: [String]) -> Raker.Arguments {
 
-        let granularityFactory = GranularityArgumentBuilder()
-        let reporterFactory = ReporterArgumentBuilder()
+        let projectArgumentBuilder = ProjectArgumentBuilder()
+        let granularityArgumentBuilder = GranularityArgumentBuilder()
+        let reporterArgumentBuilder = ReporterArgumentBuilder()
 
         do {
             let workspaceArg: OptionArgument<String> =
@@ -52,35 +53,39 @@ class ArgumentsBuilder {
                 parser.add(option: "--target", shortName: "-t", kind: String.self, usage: "The Xcode target (permitted if project is specified)")
             let modulesArg: OptionArgument<[String]> =
                 parser.add(option: "--modules", shortName: "-m", kind: [String].self, usage: "The modules to analyse (required)")
-            let validGranularities = granularityFactory.validGranularityNames.joined(separator: "|")
+            let validGranularities = granularityArgumentBuilder.validGranularityNames.joined(separator: "|")
             let granularityArg: OptionArgument<String> =
                 parser.add(option: "--granularity", shortName: "-g", kind: String.self, usage: "How to group components [\(validGranularities)] (defaults to module)")
             let verboseArg: OptionArgument<Bool> =
                 parser.add(option: "--verbose", shortName: "-v", kind: Bool.self, usage: "Verbose logging")
             let ignoreExternsArg: OptionArgument<Bool> =
                 parser.add(option: "--ignoreExterns", shortName: "-i", kind: Bool.self, usage: "Ignore dependencies external to specified modules")
-            let validReports = reporterFactory.validReportNames.joined(separator: "|")
+            let validReports = reporterArgumentBuilder.validReportNames.joined(separator: "|")
             let reportsArg: OptionArgument<[String]> =
                 parser.add(option: "--reports", shortName: "-r", kind: [String].self, usage: "One or more reports to produce on stdout [\(validReports)] (defaults to all)")
 
             let arguments = Array(arguments.dropFirst())
             let parsedArguments = try parser.parse(arguments)
 
-            let (path, xcodeBuildArguments) = try parseBuildArguments(parsedArguments: parsedArguments,
-                                                                      workspaceArg: workspaceArg,
-                                                                      projectArg: projectArg,
-                                                                      schemeArg: schemeArg,
-                                                                      targetArg: targetArg)
+            let parsedWorkspace = parsedArguments.get(workspaceArg)
+            let parsedProject = parsedArguments.get(projectArg)
+            let parsedScheme = parsedArguments.get(schemeArg)
+            let parsedTarget = parsedArguments.get(targetArg)
+
+            let (path, xcodeBuildArguments) = try projectArgumentBuilder.parse(workspace: parsedWorkspace,
+                                                                               project: parsedProject,
+                                                                               scheme: parsedScheme,
+                                                                               target: parsedTarget)
 
             guard let moduleNames = parsedArguments.get(modulesArg) else {
                 throw ArgumentsBuilderError.noModulesSpecified
             }
 
             let granularity = parsedArguments.get(granularityArg)
-            let (granularityStrategy, componentNameStrategy) = try granularityFactory.makeStrategies(granularity: granularity, path: path)
+            let (granularityStrategy, componentNameStrategy) = try granularityArgumentBuilder.makeStrategies(granularity: granularity, path: path)
 
             let reportNames = parsedArguments.get(reportsArg)
-            let reporter = try reporterFactory.makeReporter(for: reportNames)
+            let reporter = try reporterArgumentBuilder.makeReporter(for: reportNames)
 
             let isVerbose = parsedArguments.get(verboseArg) ?? false
             let ignoreExternalDependencies = parsedArguments.get(ignoreExternsArg) ?? false
@@ -107,64 +112,6 @@ class ArgumentsBuilder {
             printStdErr("Error: \(error.localizedDescription)\n")
             exitWithUsage()
         }
-    }
-
-    private func parseBuildArguments(parsedArguments: ArgumentParser.Result,
-                                     workspaceArg: OptionArgument<String>,
-                                     projectArg: OptionArgument<String>,
-                                     schemeArg: OptionArgument<String>,
-                                     targetArg: OptionArgument<String>) throws -> (String, [String]) {
-
-        var hasWorkspace = false
-        var hasScheme = false
-        var hasProject = false
-        var hasTarget = false
-        var path = ""
-        var xcodeBuildArguments = [String]()
-
-        let parsedWorkspace = parsedArguments.get(workspaceArg)
-        let parsedProject = parsedArguments.get(projectArg)
-        let parsedScheme = parsedArguments.get(schemeArg)
-        let parsedTarget = parsedArguments.get(targetArg)
-
-        if let workspace = parsedWorkspace {
-            xcodeBuildArguments.append(contentsOf: ["-workspace", workspace])
-            path = findPath(forWorkspaceOrProject: workspace)
-            hasWorkspace = true
-        }
-        if let scheme = parsedScheme {
-            xcodeBuildArguments.append(contentsOf: ["-scheme", scheme])
-            hasScheme = true
-        }
-        if let project = parsedProject {
-            xcodeBuildArguments.append(contentsOf: ["-project", project])
-            path = findPath(forWorkspaceOrProject: project)
-            hasProject = true
-        }
-        if let target = parsedTarget {
-            xcodeBuildArguments.append(contentsOf: ["-target", target])
-            hasTarget = true
-        }
-
-        if !(hasWorkspace || hasProject) {
-            throw ArgumentsBuilderError.needWorkspaceOrProject
-        }
-        if hasWorkspace && hasProject {
-            throw ArgumentsBuilderError.workspaceAndProjectSpecified
-        }
-        if hasWorkspace && !hasScheme {
-            throw ArgumentsBuilderError.missingScheme
-        }
-        if hasProject && !(hasTarget || hasScheme) {
-            throw ArgumentsBuilderError.missingTargetOrScheme
-        }
-
-        return (path, xcodeBuildArguments)
-    }
-
-    private func findPath(forWorkspaceOrProject workspaceOrProject: String) -> String {
-        let path = URL(fileURLWithPath: workspaceOrProject)
-        return path.deletingLastPathComponent().path
     }
     
     private func exitWithUsage() -> Never {
---
+++ Sources/Muck/Raker/ProjectArgumentBuilder.swift
@@ -0,0 +1,54 @@
+import Foundation
+import Utility
+
+class ProjectArgumentBuilder {
+
+    func parse(workspace: String?, project: String?, scheme: String?, target: String?) throws -> (String, [String]) {
+
+        var hasWorkspace = false
+        var hasScheme = false
+        var hasProject = false
+        var hasTarget = false
+        var path = ""
+        var xcodeBuildArguments = [String]()
+
+        if let workspace = workspace {
+            xcodeBuildArguments.append(contentsOf: ["-workspace", workspace])
+            path = findPath(forWorkspaceOrProject: workspace)
+            hasWorkspace = true
+        }
+        if let scheme = scheme {
+            xcodeBuildArguments.append(contentsOf: ["-scheme", scheme])
+            hasScheme = true
+        }
+        if let project = project {
+            xcodeBuildArguments.append(contentsOf: ["-project", project])
+            path = findPath(forWorkspaceOrProject: project)
+            hasProject = true
+        }
+        if let target = target {
+            xcodeBuildArguments.append(contentsOf: ["-target", target])
+            hasTarget = true
+        }
+
+        if !(hasWorkspace || hasProject) {
+            throw ArgumentsBuilderError.needWorkspaceOrProject
+        }
+        if hasWorkspace && hasProject {
+            throw ArgumentsBuilderError.workspaceAndProjectSpecified
+        }
+        if hasWorkspace && !hasScheme {
+            throw ArgumentsBuilderError.missingScheme
+        }
+        if hasProject && !(hasTarget || hasScheme) {
+            throw ArgumentsBuilderError.missingTargetOrScheme
+        }
+
+        return (path, xcodeBuildArguments)
+    }
+
+    private func findPath(forWorkspaceOrProject workspaceOrProject: String) -> String {
+        let path = URL(fileURLWithPath: workspaceOrProject)
+        return path.deletingLastPathComponent().path
+    }
+}
