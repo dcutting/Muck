e4b191f18079dbb457f4286cff3850f26fb675ab
Dan Cutting <dan@cutting.io>
2018-02-05 16:45:55 +0000

    Properly sort declarations by name.

--- Sources/Muck/Reporter/DeclarationReporter.swift
+++ Sources/Muck/Reporter/DeclarationReporter.swift
@@ -8,11 +8,15 @@ class DeclarationReporter: Reporter {
 
         let components = mainSequence.components.sorted { $0.name < $1.name }
         let componentReport = components.map { component -> [String] in
-            let abstracts = component.types.abstracts.sorted().map { abstract in
-                "  - [A] \(mainSequence.declarations.findName(for: abstract))"
+            let abstracts = component.types.abstracts.map { abstract in
+                mainSequence.declarations.findName(for: abstract)
+            }.sorted().map {
+                "  - [A] \($0)"
             }
-            let concretes = component.types.concretes.sorted().map { concrete in
-                "  - \(mainSequence.declarations.findName(for: concrete))"
+            let concretes = component.types.concretes.map { concrete in
+                mainSequence.declarations.findName(for: concrete)
+            }.sorted().map {
+                "  - \($0)"
             }
             return ["\(component.name)"] + abstracts + concretes
         }
